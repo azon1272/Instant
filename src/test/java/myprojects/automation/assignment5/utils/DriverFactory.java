d6fa233454a73a999a61840fa6aaa710088051c7
TheD29 <dmberezovskyi@gmail.com>
2018-07-03 13:03:27 +0300

    ubuntu

--- src/test/java/myprojects/automation/assignment5/utils/DriverFactory.java
+++ src/test/java/myprojects/automation/assignment5/utils/DriverFactory.java
@@ -47,17 +47,10 @@ public class DriverFactory {
                 return new ChromeDriver(chromeOptions);
             case "chrome":
             default:
-                String os = System.getProperty("os.name").toLowerCase();
-                if (os.contains("win")) {
-                    System.setProperty(
-                            "webdriver.chrome.driver",
-                            new File(DriverFactory.class.getResource("/chromedriver.exe").getFile()).getPath());
-                } else if (os.contains("nux") || os.contains("nix")) {
+
                     System.setProperty(
                             "webdriver.chrome.driver",
                             new File(DriverFactory.class.getResource("/chromedriver").getFile()).getPath());
-                }
-
                 return new ChromeDriver();
         }
     }
